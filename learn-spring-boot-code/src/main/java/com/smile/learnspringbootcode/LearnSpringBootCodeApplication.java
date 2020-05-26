ee91a0a537f381b1c7ed7bca4cda17c56a8f3dcc
smile <wyj950411@foxmail.com>
2020-05-26 22:53:57 +0800

    Initial commit

---
+++ learn-spring-boot-code/src/main/java/com/smile/learnspringbootcode/LearnSpringBootCodeApplication.java
@@ -0,0 +1,13 @@
+package com.smile.learnspringbootcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootCodeApplication.class, args);
+    }
+
+}
---
+++ learn-spring-boot-code/src/test/java/com/smile/learnspringbootcode/LearnSpringBootCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.learnspringbootcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
